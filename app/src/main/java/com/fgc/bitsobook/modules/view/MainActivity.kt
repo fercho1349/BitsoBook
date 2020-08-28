0e5ea6368760dfcdcf480f733487f81b79a6d986
cirith_gorgor666@hotmail.com <Binichtot1349>
2020-08-28 03:03:11 -0500

    ModelView Architecture, was added

--- app/src/main/java/com/fgc/bitsobook/modules/view/MainActivity.kt
+++ app/src/main/java/com/fgc/bitsobook/modules/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.fgc.bitsobook
+package com.fgc.bitsobook.modules.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fgc.bitsobook.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
